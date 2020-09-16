b69aecd0408185160004fdbd3ea29db5967602f4
Darius Matthews <DariustheDeveloper@gmail.com>
2020-09-16 18:48:57 -0500

    add header, balance, income expense components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import Balance from './components/Balance.jsx';
+import IncomeExp from './components/IncomeExp';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExp />
+      </div>
     </div>
   );
 }
